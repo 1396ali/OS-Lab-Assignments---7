8a49d02298e07d438b804e0d34a75439c86a925c
Ali Poorabbas <ali.saba1396@gmail.com>
2021-11-23 17:38:50 +0330

    Update Married.py

--- Married.py
+++ Married.py
@@ -4,23 +4,17 @@ boys = ['ali','reza','yasin','benyamin','mehrdad','sajjad','aidin','shahin']
 girls = ['sara','zari','neda','homa','eli','goli','mary','mina']
 
 res = []
-i = 0
 
-len_all = len(girls)
-
-while i < len_all:
+for i in range(len(boys)):
     x = random.randint(0,len(boys)-1)
     y = random.randint(0,len(girls)-1)
 
     select_boys = boys[x]
-    select_girls = girls[y]
-
-    res.append(select_boys + ' vs ' + select_girls)
-
     boys.remove(select_boys)
+    
+    select_girls = girls[y]
     girls.remove(select_girls)
 
-    i += 1
+    res.append(select_boys + " + " + select_girls)
 
-for result in res:
-    print(result)
+print(res)
